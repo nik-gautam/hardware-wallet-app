aa27d435d407f4694df32d6ed93109073de6e644
parmeet6032 <singh.pmeet752@gmail.com>
2021-11-03 14:27:25 +0530

    home and navigation tab changes

--- src/navigators/TabNavigation.js
+++ src/navigators/TabNavigation.js
@@ -5,16 +5,59 @@ import Receive from "../screens/Receive";
 import SendScreen from "../screens/send/SendScreen";
 import Settings from "../screens/Settings";
 import SendNavigation from "./SendNavigation";
+import { Entypo } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { Colours } from "../../assets/colours/Colours";
 
 const Tabs = createBottomTabNavigator();
 
 export const TabNavigation = () => {
   return (
-    <Tabs.Navigator screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="Home" component={Home} />
-      <Tabs.Screen name="Send" component={SendNavigation} />
-      <Tabs.Screen name="Receive" component={Receive} />
-      <Tabs.Screen name="Settings" component={Settings} />
+    <Tabs.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: Colours.Orange,
+      }}>
+      <Tabs.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Send"
+        component={SendNavigation}
+        options={{
+          tabBarLabel: "Send",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="upload" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Receive"
+        component={Receive}
+        options={{
+          tabBarLabel: "Receive",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="download" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarLabel: "Settings",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="settings" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs.Navigator>
   );
 };
