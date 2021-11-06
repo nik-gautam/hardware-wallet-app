44549f8fa1671d9e2daab31979e9c34586947387
parmeet6032 <singh.pmeet752@gmail.com>
2021-11-06 07:06:21 +0530

    summary and transaction complete screen

--- src/screens/send/TransactionCompleteScreen.js
+++ src/screens/send/TransactionCompleteScreen.js
@@ -1,28 +1,89 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text } from "react-native-elements";
+import { FontAwesome } from "@expo/vector-icons";
+import { Colours } from "../../../assets/colours/Colours";
 
 const TransactionCompleteScreen = ({ navigation }) => {
   return (
-    <View style={styles.screen}>
-      <Text> Transaction Complete </Text>
+    <View style={styles.container}>
+      <View>
+        <View style={styles.iconBackground}>
+          <FontAwesome name="send" size={24} color="white" />
+        </View>
+        <Text h4 style={styles.title}>
+          Transaction complete!
+        </Text>
+        <Text style={styles.text}>
+          It usually takes about 30 minutes for a transaction to be finalized.
+        </Text>
+      </View>
 
-      <Button
-        title="Go to Home"
-        onPress={() => {
-          navigation.popToTop();
-
-          navigation.navigate("Home");
-        }}
-      />
+      <View style={styles.buttonGroup}>
+        <TouchableOpacity
+          style={[styles.button, styles.transaction]}
+          onPress={() => {
+            navigation.popToTop();
+            navigation.navigate("Home");
+          }}>
+          <Text>View transaction</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.button, styles.home]}
+          onPress={() => {
+            navigation.popToTop();
+            navigation.navigate("Home");
+          }}>
+          <Text>Done</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  screen: {
+  container: {
     flex: 1,
-    justifyContent: "center",
+    marginHorizontal: 15,
+    justifyContent: "space-around",
+    // borderWidth: 1,
+    // borderColor: "black",
+  },
+  iconBackground: {
+    backgroundColor: "green",
+    alignSelf: "center",
+    padding: 15,
+    borderRadius: 50,
+  },
+  title: {
+    marginTop: 40,
+    marginVertical: 20,
+    textAlign: "center",
+  },
+  text: {
+    textAlign: "center",
+    marginHorizontal: 25,
+  },
+  button: {
+    marginVertical: 10,
+    padding: 10,
     alignItems: "center",
+    borderRadius: 15,
+    // borderColor: "blue",
+    // borderWidth: 1,
+  },
+  transaction: {
+    backgroundColor: Colours.Neutral_4,
+  },
+  home: {
+    backgroundColor: Colours.Orange,
+  },
+  buttonGroup: {
+    alignSelf: "center",
+    marginBottom: 30,
+    width: 300,
+    // borderColor: "green",
+    // borderWidth: 5,
   },
 });
 
