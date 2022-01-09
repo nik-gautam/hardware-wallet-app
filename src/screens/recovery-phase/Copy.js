a21c6437ba859a2e1355d1dc5f6023137a74fa7a
Nikhil Gautam <nikhilgautam992@gmail.com>
2022-01-10 03:02:18 +0530

    added screenshot

--- src/screens/recovery-phase/Copy.js
+++ src/screens/recovery-phase/Copy.js
@@ -1,22 +1,69 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  PermissionsAndroid,
+  Platform,
+} from "react-native";
 import { Colours } from "../../../assets/colours/Colours";
 import WordCell from "../../components/WordCell";
 import SingleButtonFilled from "./../../components/SingleButtonFilled";
 import { useDispatch, useSelector } from "react-redux";
 import { setMnemonic } from "../../reducers/onboarding";
+import ViewShot from "react-native-view-shot";
+import { useRef } from "react";
+import CameraRoll from "@react-native-community/cameraroll";
 
 const Copy = ({ navigation }) => {
   // const { mnemonic } = useSelector((state) => state.onboarding);
   const dispatch = useDispatch();
   const { mnemonic } = useSelector((state) => state.wallet);
+  const viewShotRef = useRef();
 
   let words = mnemonic;
 
   console.log(words);
 
+  const hasPermission = async () => {
+    const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
+
+    const hasPermission = await PermissionsAndroid.check(permission);
+    if (hasPermission) {
+      return true;
+    }
+
+    const status = await PermissionsAndroid.request(permission);
+    return status === "granted";
+  };
+
+  const captureSS = () => {
+    // console.log(viewShotRef.cu);
+
+    viewShotRef.current
+      .capture()
+      .then(async (uri) => {
+        console.log("Capture URI:- ", uri);
+
+        if (Platform.OS === "android" && !(await hasPermission())) {
+          return;
+        }
+
+        CameraRoll.save(uri, { type: "photo" });
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <View style={styles.container}>
+    <ViewShot
+      ref={viewShotRef}
+      // onCapture={(uri) => console.log(uri)}
+      // captureMode="mount"
+      options={{ format: "jpg", quality: 0.9 }}
+      style={styles.container}
+    >
+      {/* <View> */}
       <View style={styles.text}>
         <Text style={styles.title}>This is your recovery phrase</Text>
 
@@ -38,7 +85,11 @@ const Copy = ({ navigation }) => {
         />
       </View>
 
-      <SingleButtonFilled text="Download as PDF" style={styles.button} />
+      <SingleButtonFilled
+        text="Download as ScreenShot"
+        style={styles.button}
+        onPress={captureSS}
+      />
 
       <SingleButtonFilled
         text="Continue"
@@ -49,7 +100,8 @@ const Copy = ({ navigation }) => {
           navigation.navigate("RecoveryValidateIntro");
         }}
       />
-    </View>
+      {/* </View> */}
+    </ViewShot>
   );
 };
 
